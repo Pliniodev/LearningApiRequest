2e6a23720191ad4a430dd7db841c94dfc33c0ef1
Plinio Alencar <plinioprojetos@hotmail.com>
2024-05-18 09:34:42 -0300

    chore: config data layer

---
+++ app/src/main/java/com/pliniodev/learningapirequest/data/response/DogsResponse.kt
@@ -0,0 +1,6 @@
+package com.pliniodev.learningapirequest.data.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DogsResponse(val message: String)
--- app/src/main/java/com/pliniodev/learningapirequest/di/networkModule.kt
+++ app/src/main/java/com/pliniodev/learningapirequest/di/networkModule.kt
@@ -1,8 +1,18 @@
 package com.pliniodev.learningapirequest.di
 
+import com.pliniodev.learningapirequest.data.DogsGateway
 import com.pliniodev.learningapirequest.network.BuildRetrofit
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.create
 
 val networkModule = module {
     single { BuildRetrofit.build() }
+
+    // here we are using the Retrofit instance to create the DogsGateway,
+    // with this we communicate with the API
+    single<DogsGateway> {
+        val retrofit = get<Retrofit>()
+        retrofit.create<DogsGateway>()
+    }
 }
