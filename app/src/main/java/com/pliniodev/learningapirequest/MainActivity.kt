a9eee4134778dad5e78844cd48b3bca4e85126d6
Plinio Alencar <plinioprojetos@hotmail.com>
2024-06-08 17:02:18 -0300

    update main activity

--- app/src/main/java/com/pliniodev/learningapirequest/MainActivity.kt
+++ app/src/main/java/com/pliniodev/learningapirequest/MainActivity.kt
@@ -3,44 +3,40 @@ package com.pliniodev.learningapirequest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import com.pliniodev.learningapirequest.ui.home.HomeViewModel
 import com.pliniodev.learningapirequest.ui.theme.LearningApiRequestTheme
+import org.koin.androidx.compose.koinViewModel
+import com.pliniodev.learningapirequest.ui.component.DogCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = koinViewModel<HomeViewModel>()
+            val state = viewModel.homeState.collectAsState()
+
+            LaunchedEffect(key1 = Unit) {
+                viewModel.getDogImage()
+            }
+
             LearningApiRequestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                HomeContent(
+                    imageUrl = state.value.dogImageUrl
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearningApiRequestTheme {
-        Greeting("Android")
+    @Composable
+    private fun HomeContent(
+        imageUrl: String,
+    ) {
+        Column {
+            DogCard(url = imageUrl)
+        }
     }
 }
