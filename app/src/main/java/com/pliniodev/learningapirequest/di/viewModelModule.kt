6fcb0c1c7a85031d949cd3cf5da3bb6b10d595ee
Plinio Alencar <plinioprojetos@hotmail.com>
2024-06-08 16:26:34 -0300

    adjusts

--- app/src/main/java/com/pliniodev/learningapirequest/di/viewModelModule.kt
+++ app/src/main/java/com/pliniodev/learningapirequest/di/viewModelModule.kt
@@ -1,8 +1,9 @@
 package com.pliniodev.learningapirequest.di
 
 import com.pliniodev.learningapirequest.ui.home.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    single { HomeViewModel(repository = get()) }
+    viewModel { HomeViewModel(repository = get()) }
 }
